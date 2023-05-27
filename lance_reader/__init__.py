a328be883c58bd00f3d827a61c982210d1917f4f
Marc Garcia <garcia.marc@gmail.com>
2023-05-27 15:49:25 +0400

    Updating to use the interchange protocol, and adding parameters of to_table

--- lance_reader/__init__.py
+++ lance_reader/__init__.py
@@ -3,38 +3,58 @@ Implementation of the dataframe I/O connector API for LanceDB.
 
 The idea is that if pandas (and hopefully other libraries later)
 find an entrypoint pointing to this class in the `dataframe.io` group,
-it will automatically create `pandas.read_lance()`, `DataFrame.to_lance()`.
+it will automatically create `pandas.read_lance()`, `DataFrame.to_lance()`
+and `Series.to_lance()` functions and methods. The name `lance` in `read_lance`
+and `to_lance` is obtained from the name of the entrypoint defined in this
+project, not decided by pandas itself.
 
 This is implemented in this repo for illustration. In practice, it makes
 much more sense that this is directly implemented in the LanceDB project.
 """
+import pyarrow.interchange
 import lance
 
 
 class LanceDataFrameIO:
     @staticmethod
-    def pyarrow_reader(uri, version=None, asof=None):
+    def reader(uri,
+               version=None,
+               asof=None,
+               columns=None,
+               filter_=None,
+               nearest=None):
         """
-        Load Lance file to a pandas DataFrame.
+        Load the LanceDB dataset, and return it as a PyArrow Table. The format
+        can be anything that implements the dataframe interchange protocol.
 
-        Ref: https://eto-ai.github.io/lance/api/python/lance.html#lance.dataset
+        References
+        ----------
+        - https://eto-ai.github.io/lance/api/python/lance.html#lance.dataset
+        - https://data-apis.org/dataframe-protocol/latest/API.html
         """
-        return lance.dataset(uri, version, asof).to_table()
+        return (lance.dataset(uri,
+                              version,
+                              asof)
+                     .to_table(columns,
+                               filter_,
+                               nearest))
 
-    def pyarrow_writer(self, uri, schema=None, mode='create', max_rows_per_file=1048576, max_rows_per_group=1024):
+    def writer(self,
+               uri,
+               schema=None,
+               mode='create',
+               max_rows_per_file=1048576,
+               max_rows_per_group=1024):
         """
-        Export dataframe to Lance file.
+        Write object to a LanceDB file.
+
+        The received object must implement the dataframe interchange protocol.
 
         Ref: https://eto-ai.github.io/lance/api/python/lance.html#lance.write_dataset
         """
-        lance.write_dataset(self, uri, schema, mode, max_rows_per_file, max_rows_per_group)
-
-    # The next two methods are for illustration, but they don't really need to be implemented as pandas
-    # will fallback to `pyarrow_reader` and `pyarrow_writer` if they are not found
-
-    @staticmethod
-    def pandas_reader(uri, version=None, asof=None):
-        return LanceDataFrameIO.pyarrow_reader(uri, version, asof).to_pandas()
-
-    def pandas_writer(self, uri, schema=None, mode='create', max_rows_per_file=1048576, max_rows_per_group=1024):
-        self.pyarrow_writer(uri, schema, mode, max_rows_per_file, max_rows_per_group)
+        lance.write_dataset(data_obj=pyarrow.interchange.from_dataframe(self),
+                            uri=uri,
+                            schema=schema,
+                            mode=mode,
+                            max_rows_per_file=max_rows_per_file,
+                            max_rows_per_group=max_rows_per_group)
